5e5c9f60e6909222537556e12e4073fcd7c50582
arunsla <arunraj.softlogic@gmail.com>
2025-09-09 10:43:16 +0530

    update

--- node/fs-demo.js
+++ node/fs-demo.js
@@ -2,5 +2,5 @@ import fs from "fs";
 
 
 
-fs.writeFileSync("ar.txt" , "This is my first line in this line");
+fs.writeFileSync("ar.pdf" , "This is my first line in this line");
 fs.appendFileSync("ar.txt" , "\nThis is my second line in this line");
