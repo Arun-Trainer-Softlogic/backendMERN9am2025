f82ee5ea13c38b16bd9e2547b68a4521bbad9211
arunsla <arunraj.softlogic@gmail.com>
2025-09-30 09:05:31 +0530

    update

--- project/client/ar/src/App.jsx
+++ project/client/ar/src/App.jsx
@@ -1,33 +1,48 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name, setName] = useState("");
+  const [response, setResponse] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("http://localhost:3000/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      });
+
+      const data = await res.text();
+      setResponse(data);
+    } catch (err) {
+      setResponse("Error: " + err.message);
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Form + express </h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder='Enter Your Name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit">submit</button>
+        </form>
+
+        {response && <p style={{ marginTop: "1rem" }}>{response}</p>}
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
